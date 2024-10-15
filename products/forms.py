393da4cc993161706253690cd2b980a00377441d
jibontech <jibon.belaface@gmail.com>
2024-10-15 12:50:45 +0600

    added category crud

--- products/forms.py
+++ products/forms.py
@@ -1,2 +1,9 @@
 # pos/forms.py
 from django import forms
+from .models import Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
